e5e0a58f1fd7faba6b7e6b8aa5200030d596c954
DanyloUserGit <danylom2007.m@gmail.com>
2026-01-07 21:13:30 +0200

    build fix

--- src/componenst/ui/Modal/Modal.tsx
+++ src/componenst/ui/Modal/Modal.tsx
@@ -4,25 +4,23 @@ import ButtonClose from "../Button/ButtonClose";
 
 interface ModalI {
     children: ReactNode;
-    maxWidth: number;
     setIsOpen: (s: boolean) => void;
+    maxWidth:number;
 }
 
 export default function Modal(props: ModalI) {
-    const { children, maxWidth, setIsOpen } = props;
-
-    const modalWidthClass = maxWidth > 409 ? "w-[409px]" : "w-full";
+    const { children, setIsOpen } = props;
 
     return (
-        <div className="fixed inset-0 bg-black/75 flex items-center justify-center z-50 p-4 sm:p-0">
+        <div className="fixed inset-0 bg-black/75 flex items-center justify-center z-50">
             <div
-                className={`
+                className="
                     bg-bg dark:bg-bg-dark text-text dark:text-text-dark
-                    rounded-[20px] p-[20px]
-                    ${modalWidthClass}
-                    max-h-full
+                    w-full h-full rounded-none p-4
+                    sm:rounded-[20px] sm:max-w-[500px] sm:h-auto sm:p-6
                     overflow-y-auto
-                `}
+                    flex flex-col
+                "
             >
                 <div className="w-full flex justify-end">
                     <ButtonClose text={''} onClick={() => setIsOpen(false)} />
