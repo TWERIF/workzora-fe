2da13baaff9adad26687a99a56e6828a7699ad4b
DanyloUserGit <danylom2007.m@gmail.com>
2026-07-27 19:06:02 +0300

    27.07.2026

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/icon1.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <script
           src="https://accounts.google.com/gsi/client"
           async
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
 import Main from '@/features/main/ui/Main'
+import Head from 'next/head'
 import React from 'react'
 
 export default function MainPage() {
   return (
-    <Main />
+    <>
+      <Head>
+        <title>Workzora — сервіс для пошуку проєктів та виконавців</title>
+        <meta
+          name="description"
+          content="Workzora — платформа для пошуку проєктів, фрилансерів та спеціалістів. Публікуйте вакансії, знаходьте виконавців або отримуйте нові замовлення швидко, безпечно та зручно."
+        />
+      </Head>
+      <Main />
+    </>
   )
 }
